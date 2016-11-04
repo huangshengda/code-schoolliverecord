1c4a0c5923857a8ab5d10707c6b8e42ca38a165d
huangshengda <huangshengda@codyy.com>
2016-11-04 11:30:52 +0800

    add

--- src/main/java/com/codyy/slr/controller/Test.java
+++ src/main/java/com/codyy/slr/controller/Test.java
@@ -1,12 +1,41 @@
 package com.codyy.slr.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.codyy.slr.service.TestService;
+
 /**
  * 
  * @author huangshengda
  *
  */
 
-
+@Controller
+@RequestMapping("/test1")
 public class Test {
-
+	
+	@Autowired
+	private TestService testService;
+	
+	@RequestMapping("gethtml")
+	public String testName() throws Exception {
+		return "index";
+	}
+	
+	@RequestMapping("getMsg")
+	@ResponseBody
+	public String getMsg() {
+		return "hello codyy!";
+	}
+	
+	@RequestMapping("getNames")
+	@ResponseBody
+	public List<String> getNames() {
+		return testService.getNames();
+	}
 }
---
+++ src/main/java/com/codyy/slr/dao/TestDao1.java
@@ -0,0 +1,10 @@
+package com.codyy.slr.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component 
+public interface TestDao1 {
+	List<String> getNames();
+}
