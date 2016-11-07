5cbe63b920071d8fbed5c783e5ef22887bf711ca
renguangyuan <renguangyuan@codyy.com>
2016-11-07 10:49:14 +0800

    modify

--- src/main/java/com/codyy/slr/controller/Test.java
+++ src/main/java/com/codyy/slr/controller/Test.java
@@ -1,7 +1,5 @@
 package com.codyy.slr.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
