9ae8ee9a4ee9898816e8698ba0e3ed40b70d871b
huangshengda <huangshengda@codyy.com>
2016-11-08 15:34:57 +0800

    add

--- src/main/java/com/codyy/slr/controller/Test.java
+++ src/main/java/com/codyy/slr/controller/Test.java
@@ -1,16 +1,10 @@
 package com.codyy.slr.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.codyy.common.testUI.annotation.ApiParam;
-import com.codyy.slr.parambean.TestBean;
 import com.codyy.slr.service.TestService;
 
 /**
@@ -33,28 +27,14 @@ public class Test {
 	
 	@RequestMapping("getMsg")
 	@ResponseBody
-	public String getMsg(String name,String password) {
+	public String getMsg() {
 		return "hello codyy!";
 	}
 	
-	/**
-	 * 
-	 * @param name 
-	 * @param password
-	 * @param remember
-	 * @return
-	 */
-	
 	@RequestMapping("getNames")
 	@ResponseBody
-	public Map<String,String> getNames(TestBean test,
-			@ApiParam("√‹¬Î") String password,
-			
-			int remember) {
-		 testService.getNames();
-		 Map<String,String> map = new HashMap<String,String>();
-		 map.put("name", "lizhiwei");
-		return map;
+	public String getNames() {
+		return testService.getNames().get(0);
 	}
 	
 	
