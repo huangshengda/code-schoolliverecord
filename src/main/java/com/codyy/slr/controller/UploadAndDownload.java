eb4a57bd6677e160f8d63e8863d694dbaff4c389
bingshaowen <codyy@10.5.224.39>
2016-11-29 09:36:17 +0800

    update

--- src/main/java/com/codyy/slr/controller/UploadAndDownload.java
+++ src/main/java/com/codyy/slr/controller/UploadAndDownload.java
@@ -1,6 +1,8 @@
 package com.codyy.slr.controller;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.codyy.slr.util.ConfigUtils;
 import com.codyy.slr.util.UploadUtil;
+import com.codyy.slr.vo.Progress;
 import com.codyy.slr.vo.ReturnVoOne;
 
 @Controller
@@ -22,15 +25,16 @@ public class UploadAndDownload {
 	@ResponseBody
 	@RequestMapping("image/upload")
 	public ReturnVoOne  imageUpload(@RequestParam(value = "file", required = true) MultipartFile file,
+			@RequestParam(value = "filename", required = true) String filename,
 			@RequestParam(value = "sequence", required = true) String sequence ,HttpServletRequest request){
 		if(file == null){
 			return new ReturnVoOne(0,"文件内容为空");
 		}
 		
 		String fileName = file.getOriginalFilename();
-		File targetFile = new File(ConfigUtils.getValue("img.path") ,UploadUtil.createFilename(fileName));
+		String suffix = filename.substring(filename.lastIndexOf('.'));
+		File targetFile = new File(ConfigUtils.getValue("img.path") ,filename+suffix);
 		
-	//	saveFile = UploadUtil.createFile(UploadUtil.getImagePath(), suffix);
 		if (!targetFile.exists()) {
 			targetFile.mkdirs();
 		}
@@ -44,7 +48,7 @@ public class UploadAndDownload {
 				// 拼接返回对象
 				String result = "{\"name\":\"" + targetFile.getName() + "\", \"originalName\": \"" + fileName + "\", \"size\": "
 						+ targetFile.length() + ", \"state\": \"" + "SUCCESS" + "\", \"type\": \"" + type+", \"sequence\": " + sequence
-						+ "\", \"url\": \"" + url + "\"}";
+						+ "\"}";
 				result = result.replaceAll("\\\\", "\\\\");
 	
 				// 返回
@@ -65,9 +69,8 @@ public class UploadAndDownload {
 		}
 		
 		String fileName = file.getOriginalFilename();
-		File targetFile = new File(ConfigUtils.getValue("img.path") ,UploadUtil.createFilename(fileName));
+		File targetFile = new File(ConfigUtils.getValue("temp") ,UploadUtil.createFilename(fileName));
 		
-	//	saveFile = UploadUtil.createFile(UploadUtil.getImagePath(), suffix);
 		if (!targetFile.exists()) {
 			targetFile.mkdirs();
 		}
@@ -75,13 +78,11 @@ public class UploadAndDownload {
 				file.transferTo(targetFile);
 				// 文件类型
 				String type = "." + fileName.substring(fileName.lastIndexOf(".") + 1);
-				// 文件路径
-				String url =  request.getServerName()+ "/image/" + file.getOriginalFilename();
 	
 				// 拼接返回对象
 				String result = "{\"name\":\"" + targetFile.getName() + "\", \"originalName\": \"" + fileName + "\", \"size\": "
 						+ targetFile.length() + ", \"state\": \"" + "SUCCESS" + "\", \"type\": \"" + type+", \"sequence\": " + sequence
-						+ "\", \"url\": \"" + url + "\"}";
+						+ "\"}";
 				result = result.replaceAll("\\\\", "\\\\");
 	
 				// 返回
@@ -91,6 +92,33 @@ public class UploadAndDownload {
 			}
 		
 	}
+	
+	
+	
+	@ResponseBody
+	@RequestMapping("getUploadProgress")
+	public ReturnVoOne  getUploadProgress(@RequestParam(value = "filename", required = true) String filename,
+			@RequestParam(value = "filesize", required = true) Integer filesize,
+			@RequestParam(value = "sequence", required = true) String sequence ,HttpServletRequest request){
+		Progress progress =new Progress();
+		Long per1;
+		try {
+			Path aa = ConfigUtils.getValue("temp");
+			Files.newDirectoryStream(dir, resId + "_*.jpg");
+			File f = new File(ConfigUtils.getValue("temp")+filename);
+			if (f.exists() && f.isFile()){  
+				per1 = f.length();  
+		    }
+			
+			Double per = per1*1.0 / filesize*1.0 *100;
+			
+				return new ReturnVoOne(1,result);
+			}catch (Exception e) {
+				return new ReturnVoOne(0,"上传失败");
+			}
+		
+	}
+	
 	
 	
 	
