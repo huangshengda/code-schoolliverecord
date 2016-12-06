1a315cee3cb347c91076e3add42612a89e51f5d2
huangshengda <huangshengda@codyy.com>
2016-12-06 10:40:52 +0800

    添加注释

--- src/main/java/com/codyy/slr/entity/DmsServer.java
+++ src/main/java/com/codyy/slr/entity/DmsServer.java
@@ -1,36 +1,40 @@
 package com.codyy.slr.entity;
 
-
-
+/**
+ * 
+ * @Description: DMSServer地址bean  
+ * @author huangshengda  
+ * @date 2016年12月6日   
+ *
+ */
 public class DmsServer {
-    private String serverId;
+	private String serverId;
 
-    private String serverName;
+	private String serverName;
 
-    private String serverValue;
-  
-  
-    public String getServerId() {
-        return serverId;
-    }
+	private String serverValue;
 
-    public void setServerId(String serverId) {
-        this.serverId = serverId == null ? null : serverId.trim();
-    }
+	public String getServerId() {
+		return serverId;
+	}
 
-    public String getServerName() {
-        return serverName;
-    }
+	public void setServerId(String serverId) {
+		this.serverId = serverId == null ? null : serverId.trim();
+	}
 
-    public void setServerName(String serverName) {
-        this.serverName = serverName == null ? null : serverName.trim();
-    }
+	public String getServerName() {
+		return serverName;
+	}
 
-    public String getServerValue() {
-        return serverValue;
-    }
+	public void setServerName(String serverName) {
+		this.serverName = serverName == null ? null : serverName.trim();
+	}
 
-    public void setServerValue(String serverValue) {
-        this.serverValue = serverValue == null ? null : serverValue.trim();
-    }
+	public String getServerValue() {
+		return serverValue;
+	}
+
+	public void setServerValue(String serverValue) {
+		this.serverValue = serverValue == null ? null : serverValue.trim();
+	}
 }
