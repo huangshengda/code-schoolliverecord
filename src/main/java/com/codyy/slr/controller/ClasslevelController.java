5c1a2dcabc2a33d053bfe6903465e16ba3a85056
bingshaowen <codyy@10.5.224.39>
2016-12-06 10:52:38 +0800

    增加注释

--- src/main/java/com/codyy/slr/controller/ClasslevelController.java
+++ src/main/java/com/codyy/slr/controller/ClasslevelController.java
@@ -18,8 +18,10 @@ import com.codyy.slr.vo.ReturnVoList;
 import com.codyy.slr.vo.ReturnVoOne;
 
 /**
- * 年级
- * @author huangshengda
+ * 
+ * @Description: 年级
+ * @author bingshaowen 
+ * @date 2016年12月6日   
  *
  */
 @Controller
@@ -27,51 +29,61 @@ import com.codyy.slr.vo.ReturnVoOne;
 public class ClasslevelController {
 	@Autowired
 	ClasslevelService classlevelService;
+
 	/**
-	 * 获取年级
+	 * 
+	 * @Description: 获取年级列表
+	 * @param classlevelName
 	 * @return
+	 *
 	 */
 	@RequestMapping("list")
 	@ResponseBody
-	public ReturnVoList<Classlevel> getClasslevelList(String classlevelName){
+	public ReturnVoList<Classlevel> getClasslevelList(String classlevelName) {
 		int code = 1;
 		String msg = "操作成功";
 		List<Classlevel> classlevelList = new ArrayList<Classlevel>();
 		try {
 			classlevelList = classlevelService.getClasslevelList(classlevelName);
 		} catch (Exception e) {
-			 code = Constants.FAILED;
-			 msg = "操作失败";
+			code = Constants.FAILED;
+			msg = "操作失败";
 			e.printStackTrace();
 		}
-		return new ReturnVoList<Classlevel>(code,msg,classlevelList);
+		return new ReturnVoList<Classlevel>(code, msg, classlevelList);
 	}
-	
+
 	/**
-	 * 增加年级
+	 * 
+	 * @Description: 新增年级
+	 * @param classlevel
 	 * @return
+	 *
 	 */
 	@RequestMapping("add")
 	@ResponseBody
 	public ReturnVoOne<Classlevel> addClasslevel(Classlevel classlevel) {
-		int code ;
-		String msg ;
-		Map<String,Object> map = new HashMap<String, Object>();
+		int code;
+		String msg;
+		Map<String, Object> map = new HashMap<String, Object>();
 		try {
 			map = classlevelService.addClasslevel(classlevel);
 			code = Integer.parseInt(String.valueOf(map.get("code")));
 			msg = String.valueOf(map.get("msg"));
 		} catch (Exception e) {
-			 code = Constants.FAILED;
-			 msg = "操作失败";
+			code = Constants.FAILED;
+			msg = "操作失败";
 			e.printStackTrace();
 		}
-		return new ReturnVoOne<Classlevel>(code,msg);
+		return new ReturnVoOne<Classlevel>(code, msg);
 	}
-	
+
 	/**
-	 * 删除年级
+	 * 
+	 * @Description: 删除年级
+	 * @param classlevel
 	 * @return
+	 *
 	 */
 	@RequestMapping("del")
 	@ResponseBody
@@ -80,16 +92,20 @@ public class ClasslevelController {
 		String msg = "删除成功";
 		classlevel.setDeleteFlag("Y");
 		classlevel.setDeleteTime(new Date());
-		if(classlevelService.modifyClasslevel(classlevel)!=1){
+		if (classlevelService.modifyClasslevel(classlevel) != 1) {
 			msg = "删除失败";
 			code = Constants.FAILED;
-		};
-		return new ReturnVoOne<Classlevel>(code,msg);
+		}
+		;
+		return new ReturnVoOne<Classlevel>(code, msg);
 	}
-	
+
 	/**
-	 * 修改年级名称
+	 * 
+	 * @Description: 修改年级名称
+	 * @param classlevel
 	 * @return
+	 *
 	 */
 	@RequestMapping("update")
 	@ResponseBody
@@ -98,26 +114,30 @@ public class ClasslevelController {
 		String msg = "编辑成功";
 		try {
 			List<Classlevel> list = classlevelService.getClasslevelList(classlevel.getClasslevelName());
-			if(list.size()==0){
-				if(classlevelService.modifyClasslevel(classlevel)!=1){
+			if (list.size() == 0) {
+				if (classlevelService.modifyClasslevel(classlevel) != 1) {
 					msg = "编辑失败";
 					code = Constants.FAILED;
-				};
-			}else{
+				}
+				;
+			} else {
 				msg = "学科名称重复";
 				code = Constants.FAILED;
 			}
 		} catch (Exception e) {
-			 code = Constants.FAILED;
-			 msg = "操作失败";
+			code = Constants.FAILED;
+			msg = "操作失败";
 			e.printStackTrace();
 		}
-		return new ReturnVoOne<Classlevel>(code,msg);
+		return new ReturnVoOne<Classlevel>(code, msg);
 	}
-	
+
 	/**
-	 * 修改年级排序
+	 * 
+	 * @Description: 修改年级排序
+	 * @param classlevelIds
 	 * @return
+	 *
 	 */
 	@RequestMapping("sort")
 	@ResponseBody
@@ -126,19 +146,19 @@ public class ClasslevelController {
 		String msg = "排序成功";
 		String id[] = classlevelIds.split(",");
 		List<Classlevel> list = new ArrayList<Classlevel>();
-		for(int i=0;i<id.length;i++){
+		for (int i = 0; i < id.length; i++) {
 			Classlevel sub = new Classlevel();
 			sub.setClasslevelId(id[i]);
-			sub.setSort(i+1);
+			sub.setSort(i + 1);
 			list.add(sub);
 		}
 		try {
 			classlevelService.modifyClasslevelSort(list);
 		} catch (Exception e) {
-			 code = Constants.FAILED;
-			 msg = "排序失败";
+			code = Constants.FAILED;
+			msg = "排序失败";
 			e.printStackTrace();
 		}
-		return new ReturnVoOne<Classlevel>(code,msg);
+		return new ReturnVoOne<Classlevel>(code, msg);
 	}
 }
