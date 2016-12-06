084c4001f5471389b15fd12dcbc51a46a48ba514
huangshengda <huangshengda@codyy.com>
2016-12-06 10:40:52 +0800

    添加注释

--- src/main/java/com/codyy/slr/filter/CrossDomainFilter.java
+++ src/main/java/com/codyy/slr/filter/CrossDomainFilter.java
@@ -12,31 +12,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * @author xierongbing
+ * 
+ * @Description: 解决跨域(过滤器)  
+ * @author huangshengda  
+ * @date 2016年12月6日   
+ *
  */
-public class CrossDomainFilter implements Filter{
+public class CrossDomainFilter implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		
+
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest)request;
-		HttpServletResponse rep = (HttpServletResponse)response;
-		
-		//环境部署时 nginx中已经设置了 Access-Control-Allow-Origin 就不用加了
-		if(rep.getHeader("Access-Control-Allow-Origin") == null ){
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse rep = (HttpServletResponse) response;
+
+		// 环境部署时 nginx中已经设置了 Access-Control-Allow-Origin 就不用加了
+		if (rep.getHeader("Access-Control-Allow-Origin") == null) {
 			rep.setHeader("Access-Control-Allow-Origin", "*");
 		}
 		rep.setHeader("Access-Control-Allow-Methods", "GET,POST");
-		
-		/**
-		 * 如果请求为OPTIONS,那说明是浏览器对服务器的查询
-		 */
+
+		// 如果请求为OPTIONS,那说明是浏览器对服务器的查询
 		String method = req.getMethod();
-		if("OPTIONS".equals(method) || "options".equals(method)) {
+		if ("OPTIONS".equals(method) || "options".equals(method)) {
 			return;
 		} else {
 			chain.doFilter(request, response);
@@ -45,6 +47,6 @@ public class CrossDomainFilter implements Filter{
 
 	@Override
 	public void destroy() {
-		
+
 	}
 }
