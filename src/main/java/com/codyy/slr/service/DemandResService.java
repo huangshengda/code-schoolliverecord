0369742a2c7cce69da348879914d5a31fb6fa08a
huangshengda <huangshengda@codyy.com>
2016-12-06 17:38:12 +0800

    mod

--- src/main/java/com/codyy/slr/service/DemandResService.java
+++ src/main/java/com/codyy/slr/service/DemandResService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.codyy.slr.common.page.Page;
+import com.codyy.slr.constant.Constants;
 import com.codyy.slr.dao.DemandResMapper;
 import com.codyy.slr.util.HostConfigUtils;
 import com.codyy.slr.vo.ResourceVo;
@@ -34,7 +35,7 @@ public class DemandResService {
 	 *
 	 */
 	public Page getDemandResPageList(HttpServletRequest req, Page page) {
-		String contextpath = HostConfigUtils.getHost(req) + "/download/img";
+		String contextpath = HostConfigUtils.getHost(req) + "/download/img/" + Constants.IMG_REAL;
 		List<ResourceVo> list = demandResMapper.getDemandResPageList(page);
 		for (ResourceVo resVo : list) {
 			resVo.setThumbPath(contextpath + resVo.getThumbPath());
