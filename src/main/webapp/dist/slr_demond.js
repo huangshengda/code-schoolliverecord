f1e51c93f53796b065d61961b3e0ccdf2c844fdf
liangjifei <liangjifei@codyy.com>
2016-12-09 10:30:36 +0800

    修改访问首页。

---
+++ src/main/webapp/dist/slr_demond.js
@@ -0,0 +1,57 @@
+webpackJsonp([3,5],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	/**  加载基础CSS文件  **/
+	__webpack_require__(128);
+	/*window.Vue = require('vue');
+	window.VueResource = require('vue-resource');*/
+
+/***/ },
+
+/***/ 128:
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(129);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(86)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(true) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept(129, function() {
+				var newContent = __webpack_require__(129);
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+
+/***/ 129:
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(11)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "@charset \"utf-8\";\r\nbody{\r\n\t\r\n}\r\n.c4 {\r\n\tcolor:#444;\r\n}\r\n.c6 {\r\n\tcolor:#666;\r\n}\r\n.c9 {\r\n\tcolor:#999;\r\n}\r\n.l-left {\r\n\tdisplay:inline-block;\r\n\twidth:918px;\r\n\theight:562px;\r\n}\r\n.l-right {\r\n\twidth:281px;\r\n\tborder:1px solid #ececec;\r\n\tborder-top:1px solid #34a150;\r\n}\r\n.right-head {\r\n\theight:45px;\r\n\tline-height:45px;\r\n\tcolor:#34a150;\r\n\tpadding:0 15px;\r\n}\r\n.l-title {\r\n\tfont-size:20px;\r\n\tcolor:#222;\r\n\tline-height:35px;\r\n}\r\n.l-title span {\r\n\tmargin:0 25px;\r\n\tcolor:#666;\r\n}\r\n.l-title small {\r\n\tcolor:#666;\r\n}\r\n.l-title i {\r\n\tfont-style:normal;\r\n}\r\n/* å¬å± end*/\r\n .vod {\r\n\tmargin-top:50px;\r\n}\r\n.l-right {\r\n\tbackground-color:#fff;\r\n}\r\n.right-head {\r\n\tborder-bottom:1px solid #ececec;\r\n}\r\n.v-recommend {\r\n}\r\n.v-recommend li {\r\n\tmargin-top:20px;\r\n\tcursor:pointer;\r\n\tpadding:10px;\r\n\tcursor: pointer;\r\n}\r\n.media-left,.media-right {\r\n\tdisplay:table-cell;\r\n\tvertical-align:top;\r\n\tpadding-right:1px;\r\n}\r\n.v-recommend li:hover {\r\n\tbackground-color:#f5f8fa;\r\n}\r\n/* start */\r\n  .comment {\r\n\twidth:100%;\r\n\tdisplay:inline-block;\r\n\tborder:1px solid #ececec;\r\n\tmargin-top:30px;\r\n}\r\n.c-title {\r\n\theight:40px;\r\n\tline-height:40px;\r\n\tborder-bottom:1px solid #ececec;\r\n\tpadding-left:20px;\r\n\tbackground-color:#f7f7f7;\r\n}\r\n.c-main {\r\n\tpadding:20px;\r\n\tbackground-color:#fff;\r\n}\r\n.edit-wb-frame {\r\n\twidth:877px;\r\n\theight:120px;\r\n\tborder:1px solid #ececec;\r\n}\r\n.c-item {\r\n\tborder-bottom:1px solid #ececec;\r\n}\r\n.com-button.orange-btn {\r\n\tbackground-color:#FF9C00;\r\n\tcolor:#fff;\r\n\tpadding:2px 17px;\r\n}\r\n.textarea-ft-cont-pos {\r\n\tfont-size:12px;\r\n\tcolor:#999;\r\n\tfloat:right;\r\n\tmargin-top:3px;\r\n}\r\n.orange {\r\n\tcolor:#FE9B00;\r\n}\r\n.icon-face {\r\n\tcolor:#f1b93c;\r\n\tfont-size:20px;\r\n}\r\n.all-list {\r\n\tline-height:30px;\r\n\tborder-bottom:1px solid #ececec;\r\n}\r\n.replay {\r\n\tbackground-color:#f7f7f7;\r\n\tpadding:20px;\r\n\tmargin-left:10px;\r\n}\r\n.replay textarea {\r\n\twidth:823px;\r\n\theight:40px;\r\n}\r\n.item-Head {\r\n\tclear:both;\r\n\tmargin:20px 0;\r\n}\r\n.item-Head a {\r\n\tcolor:#999;\r\n}\r\n/*  用户评论主体部分  end*/\r\n.more {\r\n\tborder-top:1px solid #ececec;\r\n\theight:40px;\r\n\tline-height:40px;\r\n\ttext-align:center;\r\n}\r\n/* 点播课程详情  end*/", ""]);
+
+	// exports
+
+
+/***/ }
+
+});
---
+++ src/main/webapp/pages/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>校园直录播</title>
+<meta charset="UTF-8">
+<meta name="renderer" content="webkit">
+<meta name="viewport" content="width=device-width">
+<%@ include file="./_commons/meta.jsp"%>
+</head>
+<body>
+<div id="app"> hello</div>
+<%@ include file="./_commons/footer.jsp"%>
+<script type="text/javascript" src="../dist/slr_app.js" ></script>
+</body>
+</html>
