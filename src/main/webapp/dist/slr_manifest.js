c6067be3075ed3810d05e2759934bbc2c493310a
liangjifei <liangjifei@codyy.com>
2016-12-18 19:38:55 +0800

    解决冲突。

--- src/main/webapp/dist/slr_manifest.js
+++ src/main/webapp/dist/slr_manifest.js
@@ -88,7 +88,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "2eaa3391a2998628b0f5"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "e543e7ec00d8197080a5"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
