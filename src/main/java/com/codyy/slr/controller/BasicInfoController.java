9160fa19c6c26a3b55b2ea34229ec20c515eb7ee
huangshengda <huangshengda@codyy.com>
2016-12-18 18:23:43 +0800

    优化代码

--- src/main/java/com/codyy/slr/controller/BasicInfoController.java
+++ src/main/java/com/codyy/slr/controller/BasicInfoController.java
@@ -1,14 +1,11 @@
 package com.codyy.slr.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.codyy.slr.constant.Constants;
-import com.codyy.slr.entity.BasicInfo;
 import com.codyy.slr.service.BasicInfoService;
 import com.codyy.slr.vo.BasicInfoVo;
 import com.codyy.slr.vo.ReturnVoOne;
@@ -36,24 +33,13 @@ public class BasicInfoController {
 	 */
 	@RequestMapping("basicinfo/update")
 	@ResponseBody
-	public ReturnVoOne<BasicInfoVo> modifyBasicInfo(BasicInfoVo param) {
+	public ReturnVoOne<BasicInfoVo> modifyBasicInfo(BasicInfoVo param, boolean thumbFlag) {
 		ReturnVoOne<BasicInfoVo> returnVoOne = new ReturnVoOne<BasicInfoVo>();
 		try {
-			boolean flag = basicInfoService.updateBasicInfo(param);
+			boolean flag = basicInfoService.updateBasicInfo(param, thumbFlag);
 			if (flag) {
-				BasicInfoVo res = new BasicInfoVo();
-				List<BasicInfo> list = basicInfoService.getBasicInfo();
-				for (BasicInfo info : list) {
-					if ("title".equalsIgnoreCase(info.getInfoName())) {
-						res.setTitle(info.getInfoValue());
-					} else if ("logoPath".equalsIgnoreCase(info.getInfoName())) {
-						res.setLogoPath(info.getInfoValue());
-					} else {
-						res.setButtomMsg(info.getInfoValue());
-					}
-				}
-				returnVoOne.setData(res);
-
+				BasicInfoVo infoVo = basicInfoService.getBasicInfo();
+				returnVoOne.setData(infoVo);
 			} else {
 				returnVoOne.setMsg("修改基础信息失败");
 				returnVoOne.setCode(Constants.FAILED);
@@ -77,23 +63,8 @@ public class BasicInfoController {
 	public ReturnVoOne<BasicInfoVo> getBasicInfo() {
 		ReturnVoOne<BasicInfoVo> returnVoOne = new ReturnVoOne<BasicInfoVo>();
 		try {
-			BasicInfoVo param = new BasicInfoVo();
-			List<BasicInfo> list = basicInfoService.getBasicInfo();
-			for (BasicInfo info : list) {
-				if ("title".equalsIgnoreCase(info.getInfoName())) {
-					param.setTitle(info.getInfoValue());
-				} else if ("logoPath".equalsIgnoreCase(info.getInfoName())) {
-					String logoPath = info.getInfoValue();
-					if (logoPath.startsWith("/public")) {
-						param.setLogoPath(Constants.ROOT_SERVER + logoPath);
-					} else {
-						param.setLogoPath(Constants.ROOT_SERVER + "/download/img" + Constants.PATH_SEPARATOR + Constants.IMG_REAL + logoPath);
-					}
-				} else {
-					param.setButtomMsg(info.getInfoValue());
-				}
-			}
-			returnVoOne.setData(param);
+			BasicInfoVo infoVo = basicInfoService.getBasicInfo();
+			returnVoOne.setData(infoVo);
 		} catch (Exception e) {
 			returnVoOne.setMsg("获取基础信息失败");
 			returnVoOne.setCode(Constants.FAILED);
