c377f9d70d7889d42c171435b08393abe288615f
bingshaowen <codyy@10.5.224.39>
2017-01-16 11:13:48 +0800

    update

--- src/main/java/com/codyy/slr/controller/UserController.java
+++ src/main/java/com/codyy/slr/controller/UserController.java
@@ -402,7 +402,7 @@ public class UserController {
 		try {
 			OutputStream out = response.getOutputStream();
 			String path = request.getServletContext().getRealPath("/");
-			File file = new File(path, Constants.TEMP + File.separator + fileName);
+			File file = new File(Constants.TEMP + File.separator + fileName);
 			InputStream in = new FileInputStream(file);
 			StreamUtils.copy(in, out);
 			file.delete();
